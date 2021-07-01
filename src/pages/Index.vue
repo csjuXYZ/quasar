98519777435ad9d2601930d451a47bff133737ee
陈述句 <hoc0415@foxmail.com>
2021-07-02 00:22:53 +0800

    fix

---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn @click="$q.notify('My message')" color="primary" label="Show a notification" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    let s = ref(5);
+    const inc = () => {
+      s.value++;
+    };
+    return { s, inc };
+  },
+});
+</script>
